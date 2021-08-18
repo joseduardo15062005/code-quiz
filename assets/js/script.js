c59eccff28f79350baa95a5f59abca3967b2ca30
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-08-17 22:42:48 -0700

    add function checkAnswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,52 @@
 const timer = document.getElementById("timer");
 const sectionWelcome = document.getElementById("welcome");
+
 const sectionQuestion = document.getElementById("question");
+const questionTitle = document.getElementById("questionTitle");
+const questionOptions = document.getElementById("questionOptions");
 const startQuizButton = document.getElementById("startQuiz");
 
-question = {
-  title: "",
-  option1: "",
-  option2: "",
-  option3: "",
-  option4: "",
-  correctOption: "",
-};
+const questions = [
+  {
+    title: "Question #1 ?",
+    options: ["string", "int", "alert", "number"],
+    correctOption: "string",
+  },
+  {
+    title: "Question #2 ?",
+    options: ["string", "int", "alert", "number"],
+    correctOption: "JS",
+  },
+  {
+    title: "Question #3 ?",
+    options: ["string", "int", "alert", "number"],
+    correctOption: "string",
+  },
+  {
+    title: "Question #4 ?",
+    options: ["string", "int", "alert", "number"],
+    correctOption: "string",
+  },
+];
 
 function startQuiz() {
   sectionWelcome.classList.add("display-none");
   sectionQuestion.classList.remove("display-none");
+  //TODO: Show First Question
+  const question = questions[0];
+  questionTitle.textContent = question.title;
+  let n = 1;
+  for (var option of question.options) {
+    const liOption = document.createElement("li");
+    liOption.addEventListener("click", checkAnswer);
+    liOption.textContent = `${n}. ${option}`;
+    questionOptions.appendChild(liOption);
+    n++;
+  }
+}
+
+function checkAnswer(event) {
+  console.log(event.target.outerText);
 }
 
 startQuizButton.addEventListener("click", startQuiz);
