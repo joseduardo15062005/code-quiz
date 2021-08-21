b2a9261ff3424a5083d35aade199f502721201a2
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-08-21 11:15:43 -0700

    move questions array to a separate file

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+const questions = [
+  {
+    title: "Question #1 ?",
+    options: ["string", "int", "alert", "number"],
+    correctOption: "string",
+  },
+  {
+    title: "Question #2 ?",
+    options: ["JS", "HTML", "CSS", "OTHER"],
+    correctOption: "JS",
+  },
+  {
+    title: "Question #3 ?",
+    options: ["string", "int", "alert", "number"],
+    correctOption: "string",
+  },
+  {
+    title: "Question #4 ?",
+    options: ["string", "int", "alert", "number"],
+    correctOption: "string",
+  },
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,29 +6,6 @@ const questionTitle = document.getElementById("questionTitle");
 const questionOptions = document.getElementById("questionOptions");
 const startQuizButton = document.getElementById("startQuiz");
 
-const questions = [
-  {
-    title: "Question #1 ?",
-    options: ["string", "int", "alert", "number"],
-    correctOption: "string",
-  },
-  {
-    title: "Question #2 ?",
-    options: ["JS", "HTML", "CSS", "OTHER"],
-    correctOption: "JS",
-  },
-  {
-    title: "Question #3 ?",
-    options: ["string", "int", "alert", "number"],
-    correctOption: "string",
-  },
-  {
-    title: "Question #4 ?",
-    options: ["string", "int", "alert", "number"],
-    correctOption: "string",
-  },
-];
-
 function startQuiz() {
   sectionWelcome.classList.add("display-none");
   sectionQuestion.classList.remove("display-none");
