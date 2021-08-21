74df7bfa555cd3a8414b20f438038a2e2e062a2f
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-08-21 11:49:16 -0700

    add id to each question

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,20 +1,24 @@
 const questions = [
   {
+    id: 1,
     title: "Question #1 ?",
     options: ["string", "int", "alert", "number"],
     correctOption: "string",
   },
   {
+    id: 2,
     title: "Question #2 ?",
     options: ["JS", "HTML", "CSS", "OTHER"],
     correctOption: "JS",
   },
   {
+    id: 3,
     title: "Question #3 ?",
     options: ["string", "int", "alert", "number"],
     correctOption: "string",
   },
   {
+    id: 4,
     title: "Question #4 ?",
     options: ["string", "int", "alert", "number"],
     correctOption: "string",
